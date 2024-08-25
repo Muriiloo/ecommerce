c4ee5bb888fc09f5173403958e0e46a66f0f6d03
Murilo <murilooliveira110305@gmail.com>
2024-08-25 17:20:51 -0300

    feat: listing api products

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,6 +1,35 @@
 const Cart = () => {
     return ( 
-        <h1>Cart</h1>
+        <div className="w-full max-w-7xl mx-auto">
+            <h1 className="text-2xl font-medium text-center my-4 ">Meu carrinho</h1>
+
+            <section className="flex items-center justify-between border-b-2 border-gray-300">
+                <img className="w-28"
+                src="https://images.tcdn.com.br/img/img_prod/167552/fone_de_ouvido_apple_airpods_pro_mwp22be_a_13305_1_49b712f1e0c3353c688e35bd6034170e.jpg"
+                alt="Logo do produto" />
+
+                <strong>Preço: R$100,00</strong>
+
+                <div className="flex gap-3 items-center justify-center">
+                    <button className="bg-slate-600 px-2 rounded text-white font-medium  flex items-center justify-center">
+                        -
+                    </button>
+                    <p>2</p>
+                    <button className="bg-slate-600 px-1.5 rounded text-white font-medium  flex items-center justify-center">
+                        +
+                    </button>
+                </div>
+
+
+                <strong className="float-right">
+                    Subtotal: R$100,00
+                </strong>
+
+            </section>
+
+            <p className="mt-4 font-bold">Total: R$100,00</p>
+
+        </div>
      );
 }
  
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,29 @@
 import { BsCartPlus } from "react-icons/bs";
+import { useState, useEffect } from "react";
+import { api } from "../../api";
+
+interface ProductsProps {
+    id: number;
+    title: string;
+    description: string;
+    price: number;
+    cover: string;
+}
+
 
 const Home = () => {
+
+    const [products, setProducts] = useState<ProductsProps[]>([]);
+
+    useEffect(()=>{
+        async function getProducts(){
+            const response = await api.get("/products");
+            setProducts(response.data);
+        }
+
+        getProducts();
+    },[])
+
   return (
     <div>
       <main className="w-full max-w-7xl px-4 mx-auto">
@@ -12,21 +35,26 @@ const Home = () => {
         </h1>
 
         <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-5">
-          <section className="w-full">
+          {products.map( (product) => (
+            <section className="w-full" key={product.id}>
             <img
               className="w-full rounded-lg max-h-70 mb-2"
-              src="https://images.tcdn.com.br/img/img_prod/167552/fone_de_ouvido_apple_airpods_pro_mwp22be_a_13305_1_49b712f1e0c3353c688e35bd6034170e.jpg"
-              alt="Logo do produto"
+              src={product.cover}
+              alt={product.title}
             />
-            <p className="font-medium mt-1 mb-2">Airpods Pro</p>
+            <p className="font-medium mt-1 mb-2">{product.title}</p>
 
             <div className="flex gap-3 items-center">
-              <strong className="text-zinc-700/90">R$ 1000</strong>
+              <strong className="text-zinc-700/90">{product.price.toLocaleString("pt-BR",{
+                style: "currency",
+                currency: "BRL"
+              })}</strong>
               <button className="bg-zinc-900 p-1 rounded">
                 <BsCartPlus size={20} color="#FFF" />
               </button>
             </div>
           </section>
+          ))}
         </div>
       </main>
     </div>
